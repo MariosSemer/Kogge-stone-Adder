44341b67678f9a24dcf55fb5cc621ce9a4843a0d
MARIOS SEMERTZAKIS <131589570+MariosSemer@users.noreply.github.com>
2025-06-14 16:11:12 +0300

    Update Gray_Cell.vhd

--- KSA/Gray_Cell.vhd
+++ KSA/Gray_Cell.vhd
@@ -1,3 +1,14 @@
+----------------------------------------------------------------------------------
+
+-- Module Name: Gray Cell
+-- Project Name: N-bit Kogge-Stone Adder
+-- Target Devices: Artix-7
+-- Description:  This file implements the gray cell, which computes only the generate
+--		 signal by combining the generate and propagate signals from two input pairs
+-- Author : Semertzakis Marios
+
+----------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 
