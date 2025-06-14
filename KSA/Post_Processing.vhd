23dbdbe28d3c928db805c4de4ac6833898793cb9
MARIOS SEMERTZAKIS <131589570+MariosSemer@users.noreply.github.com>
2025-06-14 16:12:49 +0300

    Update Post_Processing.vhd

--- KSA/Post_Processing.vhd
+++ KSA/Post_Processing.vhd
@@ -1,3 +1,15 @@
+----------------------------------------------------------------------------------
+
+-- Module Name: Post Processing Unit
+-- Project Name: N-bit Kogge-Stone Adder
+-- Target Devices: Artix-7
+-- Description:  This file implements the post process, which computes the final sum 
+-- 		 bits by XORing the propagate signal with the corresponding carry-in 
+-- 		 for each bit.
+-- Author : Semertzakis Marios
+
+----------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 
